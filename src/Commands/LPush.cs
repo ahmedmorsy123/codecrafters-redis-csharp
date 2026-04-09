a341ef9f03f8c91f4f7c864df7c10c2f1fcce984
ahmed morsy <morsy0551@gmail.com>
2026-04-09 23:51:51 +0200

    codecrafters submit [skip ci]

---
+++ src/Commands/LPush.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codecrafters_redis.src.Commands
+{
+    public class LPush : ICommand
+    {
+        public string Name => "LPUSH";
+        public Task<string> ExecuteAsync(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                return Task.FromResult(RespEncoder.EncodeError("wrong number of arguments for 'LPUSH' command"));
+            }
+            string key = args[0];
+            string[] values = args.Skip(1).ToArray();
+            long newLength = StoreProvider.Instance.LPush(key, values);
+            return Task.FromResult(RespEncoder.EncodeInteger((int)newLength));
+        }
+    }
+}
