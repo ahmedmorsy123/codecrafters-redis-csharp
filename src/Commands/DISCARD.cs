d86d67dbe9f37ab79436695f9bd655bf37ea3342
ahmed morsy <morsy0551@gmail.com>
2026-04-11 01:52:14 +0200

    codecrafters submit [skip ci]

---
+++ src/Commands/DISCARD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codecrafters_redis.src.Commands
+{
+    public class DISCARD : ICommand
+    {
+        public string Name => "DISCARD";
+
+        public Task<string> ExecuteAsync(string[] args, ClientSession session)
+        {
+            if (!session.InTransaction)
+            {
+                return Task.FromResult(RespEncoder.EncodeError("ERR DISCARD without MULTI"));
+            }
+
+            session.ResetTransaction();
+            return Task.FromResult(RespEncoder.EncodeSimpleString("OK"));
+        }
+    }
+}
