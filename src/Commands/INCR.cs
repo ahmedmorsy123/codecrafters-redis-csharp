0b048f203acbee05aaa2261f47ad9b5d53146095
ahmed morsy <morsy0551@gmail.com>
2026-04-11 00:50:51 +0200

    codecrafters submit [skip ci]

---
+++ src/Commands/INCR.cs
@@ -0,0 +1,38 @@
+﻿using codecrafters_redis.src.RedisValues;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codecrafters_redis.src.Commands
+{
+    public class INCR : ICommand
+    {
+        public string Name => "INCR";
+
+        public Task<string> ExecuteAsync(string[] args)
+        {
+            if (args.Length != 1)
+                return Task.FromResult(RespEncoder.EncodeError("wrong number of arguments for 'INCR' command"));
+
+            string key = args[0];
+            RedisString value = StoreProvider.Instance.GetOrCreate<RedisString>(key, () => new RedisString("1"));
+
+            int intVal;
+            if(int.TryParse(value.Value, out intVal))
+            {
+                intVal++;
+                value = new RedisString(intVal.ToString());
+                StoreProvider.Instance.SetEntry(key, value);
+            }
+            else
+            {
+                // value is not an int
+            }
+
+
+            StoreProvider.Instance.NotifyKeyChanged(key);
+
+            return Task.FromResult(RespEncoder.EncodeInteger(intVal));
+        }
+    }
+}
