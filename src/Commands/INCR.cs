4624ac1c31d75c9a843c5a1f08261c305a9f6a00
ahmed morsy <morsy0551@gmail.com>
2026-04-11 00:59:48 +0200

    codecrafters submit [skip ci]

--- src/Commands/INCR.cs
+++ src/Commands/INCR.cs
@@ -15,7 +15,7 @@ namespace codecrafters_redis.src.Commands
                 return Task.FromResult(RespEncoder.EncodeError("wrong number of arguments for 'INCR' command"));
 
             string key = args[0];
-            RedisString value = StoreProvider.Instance.GetOrCreate<RedisString>(key, () => new RedisString("1"));
+            RedisString value = StoreProvider.Instance.GetOrCreate<RedisString>(key, () => new RedisString("0"));
 
             int intVal;
             if(int.TryParse(value.Value, out intVal))
