8516bc023be49dc77ea5f582585534c51ab21c3c
ahmed morsy <morsy0551@gmail.com>
2026-04-11 16:30:29 +0200

    codecrafters submit [skip ci]

--- src/Resp/RespEncoder.cs
+++ src/Resp/RespEncoder.cs
@@ -101,7 +101,7 @@ namespace codecrafters_redis.src.Resp
             byte[] result = new byte[header.Length + rdbData.Length + trailer.Length];
             Buffer.BlockCopy(header, 0, result, 0, header.Length);
             Buffer.BlockCopy(rdbData, 0, result, header.Length, rdbData.Length);
-            Buffer.BlockCopy(trailer, 0, result, header.Length + rdbData.Length, trailer.Length);
+            //Buffer.BlockCopy(trailer, 0, result, header.Length + rdbData.Length, trailer.Length);
 
             return result;
         }
