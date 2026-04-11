389ca61ee8228de174101b8301d62605e6f64507
ahmed morsy <morsy0551@gmail.com>
2026-04-11 15:16:58 +0200

    codecrafters submit [skip ci]

--- src/Commands/PSYNC.cs
+++ src/Commands/PSYNC.cs
@@ -9,7 +9,7 @@ namespace codecrafters_redis.src.Commands
         public Task<string> ExecuteAsync(string[] args, ClientSession session)
         {
             // args[0] is the replication ID, args[1] is the offset
-            return Task.FromResult(RespEncoder.EncodeBulkString("FULLRESYNC " + ServerInfo.ReplId + " 0"));
+            return Task.FromResult(RespEncoder.EncodeSimpleString("FULLRESYNC " + ServerInfo.ReplId + " 0"));
         }
     }
 }
