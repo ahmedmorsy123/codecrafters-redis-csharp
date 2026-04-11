9818fa552c30089e87fd24e892ce1b548a9992f6
ahmed morsy <morsy0551@gmail.com>
2026-04-11 14:15:44 +0200

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -14,8 +14,9 @@ if (ServerInfo.ReplicaOfHost is not null && ServerInfo.ReplicaOfPort is not null
         {
             await using var master = new MasterClient();
             await master.ConnectAsync(ServerInfo.ReplicaOfHost, ServerInfo.ReplicaOfPort.Value);
-            string response = await master.SendAndReceiveAsync(["PING"]);
-            Console.Error.WriteLine($"Master response is [{response}]");
+            await master.SendAndReceiveAsync(["PING"]);
+            await master.SendAsync(["REPLCONF", "listening-port", ServerInfo.ReplicaOfPort.ToString()!]);
+            await master.SendAsync(["REPLCONF", "capa", "psync2"]);
         }
         catch (Exception ex)
         {
