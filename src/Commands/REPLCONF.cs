b6924cae8e6bca0f7ec768a3681d177dfbc1cd4b
ahmed morsy <morsy0551@gmail.com>
2026-04-11 15:12:09 +0200

    codecrafters submit [skip ci]

---
+++ src/Commands/REPLCONF.cs
@@ -0,0 +1,18 @@
+﻿using codecrafters_redis.src.Client;
+using codecrafters_redis.src.Resp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codecrafters_redis.src.Commands
+{
+    public class REPLCONF : ICommand
+    {
+        public string Name => "REPLCONF";
+
+        public Task<string> ExecuteAsync(string[] args, ClientSession session)
+        {
+            return Task.FromResult(RespEncoder.EncodeSimpleString("OK"));
+        }
+    }
+}
