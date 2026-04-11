ae68ca4a8e3a124e84632becfa348fba42816426
ahmed morsy <morsy0551@gmail.com>
2026-04-11 15:15:38 +0200

    codecrafters submit [skip ci]

---
+++ src/Commands/PSYNC.cs
@@ -0,0 +1,15 @@
+﻿using codecrafters_redis.src.Client;
+using codecrafters_redis.src.Resp;
+
+namespace codecrafters_redis.src.Commands
+{
+    public class PSYNC : ICommand
+    {
+        public string Name => "PSYNC";
+        public Task<string> ExecuteAsync(string[] args, ClientSession session)
+        {
+            // args[0] is the replication ID, args[1] is the offset
+            return Task.FromResult(RespEncoder.EncodeBulkString("FULLRESYNC " + ServerInfo.ReplId + " 0"));
+        }
+    }
+}
--- src/ServerInfo.cs
+++ src/ServerInfo.cs
@@ -4,9 +4,10 @@
     {
         public static int Port { get; set; } = 6379;
         public static string Role { get; set; } = "master";
+        public static string ReplId { get; set; } = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
         public static string? ReplicaOfHost { get; set; }
         public static int? ReplicaOfPort { get; set; }
-        public static string MasterReplId { get; set; } = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+        public static string MasterReplId { get; set; } = "?";
         public static int MasterReplOffset { get; set; } = 0;
 
         public static string GetInfoLine(string key)
