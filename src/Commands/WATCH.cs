c782deef78375f126c6e14cc6c7d6d99ee98c61f
ahmed morsy <morsy0551@gmail.com>
2026-04-11 02:26:09 +0200

    codecrafters submit [skip ci]

---
+++ src/Commands/WATCH.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codecrafters_redis.src.Commands
+{
+    public class WATCH : ICommand
+    {
+        public string Name => "WATCH";
+
+        public Task<string> ExecuteAsync(string[] args, ClientSession session)
+        {
+            // Implementation for WATCH command
+            return Task.FromResult(RespEncoder.EncodeSimpleString("OK"));
+        }
+    }
+}
