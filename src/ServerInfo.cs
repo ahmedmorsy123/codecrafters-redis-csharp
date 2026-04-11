5b85df368a03ea4a0fc18c715781e0569517cb6e
ahmed morsy <morsy0551@gmail.com>
2026-04-11 15:18:25 +0200

    codecrafters submit [skip ci]

--- src/ServerInfo.cs
+++ src/ServerInfo.cs
@@ -7,7 +7,7 @@
         public static string ReplId { get; set; } = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
         public static string? ReplicaOfHost { get; set; }
         public static int? ReplicaOfPort { get; set; }
-        public static string MasterReplId { get; set; } = "?";
+        public static string MasterReplId { get; set; } = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
         public static int MasterReplOffset { get; set; } = 0;
 
         public static string GetInfoLine(string key)
