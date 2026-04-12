5f5e4c15d9ff05a7ba2c13ebdbc60192762c3286
ahmed morsy <morsy0551@gmail.com>
2026-04-12 19:24:24 +0200

    codecrafters submit [skip ci]

--- src/Commands/SUBSCRIBE.cs
+++ src/Commands/SUBSCRIBE.cs
@@ -23,7 +23,7 @@ namespace codecrafters_redis.src.Commands
 
             string channel = args[0];
             int subscriberCount = ChannelsManger.GetOrCreateChannel(channel).Subscribe(session);
-            await session.SendStringAsync(RespEncoder.EncodeArray(new string[] { "subscribe", channel, subscriberCount.ToString() }));
+            await session.SendStringAsync(RespEncoder.EncodeArray(new string[] { "subscribe", channel, RespEncoder.EncodeInteger(subscriberCount) }));
         }
     }
 }
