2ade8bfeb2c3c3dd00ccbacb88a5e7405e7476cd
ahmed morsy <morsy0551@gmail.com>
2026-04-12 19:22:51 +0200

    codecrafters submit [skip ci]

---
+++ src/Channels/ChannelsManger.cs
@@ -0,0 +1,80 @@
+﻿using codecrafters_redis.src.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codecrafters_redis.src.Channels
+{
+    public static class ChannelsManger
+    {
+        private static readonly Dictionary<string, Channel> channels = new Dictionary<string, Channel>();
+
+        public static Channel GetOrCreateChannel(string name)
+        {
+            lock (channels)
+            {
+                if (!channels.TryGetValue(name, out var channel))
+                {
+                    channel = new Channel();
+                    channels[name] = channel;
+                }
+                return channel;
+            }
+        }
+
+        public static void RemoveChannel(string name)
+        {
+            lock (channels)
+            {
+                channels.Remove(name);
+            }
+        }
+        
+    }
+
+    public class Channel
+    {
+        private readonly HashSet<ClientSession> subscribers = new HashSet<ClientSession>();
+
+        public int SubscriberCount
+        {
+            get
+            {
+                lock (subscribers)
+                {
+                    return subscribers.Count;
+                }
+            }
+        }
+        public void Publish(string message)
+        {
+            var subscribersCopy = GetSubscribers();
+            foreach (var subscriber in subscribersCopy)
+            {
+                subscriber.SendStringAsync(message);
+            }
+        }
+        public int Subscribe(ClientSession session)
+        {
+            lock (subscribers)
+            {
+                subscribers.Add(session);
+                return subscribers.Count;
+            }
+        }
+        public void Unsubscribe(ClientSession session)
+        {
+            lock (subscribers)
+            {
+                subscribers.Remove(session);
+            }
+        }
+        public IEnumerable<ClientSession> GetSubscribers()
+        {
+            lock (subscribers)
+            {
+                return subscribers.ToArray();
+            }
+        }
+    }
+}
---
+++ src/Commands/SUBSCRIBE.cs
@@ -0,0 +1,29 @@
+﻿using codecrafters_redis.src.Channels;
+using codecrafters_redis.src.Client;
+using codecrafters_redis.src.Resp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codecrafters_redis.src.Commands
+{
+    public class SUBSCRIBE : ICommand
+    {
+        public string Name => "SUBSCRIBE";
+
+        public bool IsWrite => false;
+
+        public async Task ExecuteAsync(string[] args, ClientSession session)
+        {
+            if (args.Length != 1)
+            {
+                await session.SendStringAsync("-ERR wrong number of arguments for 'SUBSCRIBE' command\r\n");
+                return;
+            }
+
+            string channel = args[0];
+            int subscriberCount = ChannelsManger.GetOrCreateChannel(channel).Subscribe(session);
+            await session.SendStringAsync(RespEncoder.EncodeArray(new string[] { "subscribe", channel, subscriberCount.ToString() }));
+        }
+    }
+}
