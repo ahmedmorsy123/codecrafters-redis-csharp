0fa4cad5bbc0daebaaa6985a5a5d80ddabd26e4e
ahmed morsy <morsy0551@gmail.com>
2026-04-13 02:32:58 +0200

    codecrafters submit [skip ci]

--- src/RedisValues/RedisSortedSet.cs
+++ src/RedisValues/RedisSortedSet.cs
@@ -224,7 +224,7 @@ namespace codecrafters_redis.src.RedisValues
 
         public static double calculateDistanceUsingHaversine(double lat1, double lon1, double lat2, double lon2)
         {
-            var R = 6372797.560856 / 1000; // In kilometers
+            var R = 6372797.560856; 
             var dLat = toRadians(lat2 - lat1);
             var dLon = toRadians(lon2 - lon1);
             lat1 = toRadians(lat1);
